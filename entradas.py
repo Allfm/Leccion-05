10ce20d4042794757a89e63920b7b0b517cb6279
Allfm <124815621+Allfm@users.noreply.github.com>
2023-02-23 02:43:15 +0000

    Leccion 05

---
+++ entradas.py
@@ -0,0 +1,7 @@
+nombre = input("¿cual es su nombre?")
+apellido = input("¿cual es su apellido?")
+edad = input("¿cual es su edad?")
+print(nombre)
+print(apellido)
+print(edad)
+
--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello")
+#for numero in range (50, 101, 5):
+   #print( numero)
+#digitos = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+#print(min(digitos))
+#print(max(digitos))
+#print(sum(digitos))
+
+
+#numeros = list(range(1, 1000001))
+#print(min(numeros))
+#print(max(numeros))
+#print(sum(numeros))
+
+
+#for numero in range(1, 31):
+ #print(numero*3)
+  
+for numero in range(1, 31):
+ print(f"{numero} * 5 = {numero * 5}")
